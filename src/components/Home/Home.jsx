a53a0299671b9f0ae12789d8955b559c70103944
LudmilaZhuleha <zhuleha@lir.by>
2022-09-13 17:08:19 +0300

    Lesson-38: changed styles for Home component.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import {useParams} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
 import Button from '../Button/Button';
-import Card from "../Card/Card";
+import Container from '../Container/Container';
 
 const Home = () => {
   const [singleHome, setSingleHome] = useState(null);
@@ -19,19 +19,34 @@ const Home = () => {
   )
 
   return (
-    <div>
+    <Container>
       {singleHome && (
         <>
-          <Card
-            imageUrl={singleHome.imageUrl}
-            name={singleHome.name}
-            city={singleHome.city}
-            country={singleHome.country}
-          />
+          <div className="home-wrapper">
+            <div className="home-card">
+              <img
+                width="296"
+                height="296"
+                className="home-card-image"
+                src={singleHome.imageUrl}
+                alt={singleHome.name}
+              />
+            </div>
+            <div className="reviews-card-description">
+              <h3 className="home-image-name">{singleHome.name}</h3>
+              <p className="home-image-location">
+                {singleHome.city}, {singleHome.country}
+              </p>
+              <div className="reviews-card_rating">9,4</div>
+              <p className="reviews-card_place">Excellent</p>
+              <p className="reviews-card_ratinginfo">1324 reviews</p>
+              <Button title="Check accommodation" className="home-card-button" />
+            </div>
+          </div>
           <Button className="button-lg" title="Back Home" onClick={()=>navigate('/')} />
         </>
       )}
-    </div>
+    </Container>
   );
 };
 export default Home;
