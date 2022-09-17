b059ad494321ae4e6ed751f3b72e9823cfe99632
LudmilaZhuleha <zhuleha@lir.by>
2022-09-17 23:05:37 +0300

    Lesson-38: some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
 import Home from "./components/Home/Home";
@@ -11,9 +11,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index  element={<App />}/>
+        <Route index element={<App />} />
         <Route path="/" element={<LayoutHomes />}>
-          <Route path="/:id" element={<Home />}/>
+          <Route path="/:id" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
