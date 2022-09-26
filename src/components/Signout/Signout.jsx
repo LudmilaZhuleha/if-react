aa257e09d23655f1015d2ecdb663ee34e680cf8b
LudmilaZhuleha <zhuleha@lir.by>
2022-09-26 12:01:16 +0300

    Lesson-39: some changes.

--- src/components/Signout/Signout.jsx
+++ src/components/Signout/Signout.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Icon from "../Icon/Icon";
-import './Signout.css';
+import "./Signout.css";
 
 const Signout = () => {
   const navigate = useNavigate();
   return (
-    <button onClick={()=>{navigate('/')}} type="text" className="signout-btn">
+    <button
+      onClick={() => {
+        navigate("/");
+      }}
+      type="text"
+      className="signout-btn"
+    >
       <Icon id="signout" width="20" height="18" />
-      <span style={{marginLeft: '16px'}}>Sign out</span>
+      <span style={{ marginLeft: "16px" }}>Sign out</span>
     </button>
   );
 };
